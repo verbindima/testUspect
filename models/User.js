d503b5320700b99001b78130b88193aac19c31c5
Dmitriy <verbindima22@gmail.com>
2021-11-04 19:01:32 +0300

    Add model User.js

---
+++ models/User.js
@@ -0,0 +1,11 @@
+import { Schema, model } from 'mongoose';
+
+const User = new Schema({
+    login: {type: String, unique: true, required: true }, 
+    password: {type: String, required: true},
+    phone: {type: String, required: true},
+    sex: {type: String},
+    isAdmin: {type: Boolean, default: false}
+})
+
+export default model('User', User)
