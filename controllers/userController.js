146b7a18a5c5722de934d12670800877c9c76732
Dmitriy <verbindima22@gmail.com>
2021-11-05 00:01:15 +0300

    Add register userController and fix mistakes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,24 +1,50 @@
+import { validationResult } from 'express-validator';
+import bcrypt from 'bcryptjs';
+import User from "../models/User.js";
+
+
 class userController {
 
     async register(req,res) {
         try {
-            
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(401).json({message: "Ошибка валидации при регистрации", errors})
+            }
+            const {login, password, phone, sex, isAdmin} = req.body
+            const candidate = await User.findOne({login})
+            if (candidate) {
+                return res.status(400).json({message: 'Пользователь с таким именем уже существует'})
+            }
+            const hashPassword = bcrypt.hashSync(password, 7); //Hash password
+            const user = new User({login, password: hashPassword, phone, sex, isAdmin})
+            await user.save()
             return res.json({message: "Пользователь успешно зарегистрирован"});
         } catch (e) {
             console.log(e)
-            res.status(400).json({message: 'registration Error'}) 
+            res.status(401).json({message: 'registration Error'}) 
         }
     }
     
     async login(req,res) {
         try {
             const {login, password} = req.body
+            return res.status(400).json({message: 'login Error'})
         } catch (e) {
            
             res.status(400).json({message: 'login Error'})
         }
     }
     async logout(req,res) {
+        try {
+
+
+
+        } catch(e) {
+            res.status(400).json({message: 'logout Error'})
+        }
+    }
+    async updateUser(req,res) {
         try {
 
 
