b763f01cdcdffb2aec5f143c8e3772e9e8367406
Dmitriy <verbindima22@gmail.com>
2021-11-07 16:30:20 +0300

    patch: update index.js to dotenv + add cookieParser + mod package + add .env

--- api/v1/index.js
+++ api/v1/index.js
@@ -1,18 +1,22 @@
+import * as dotenv from 'dotenv'
+dotenv.config({ path: './.env' })
 import express, { json } from 'express';
 import mongoose from 'mongoose';
 import userRouter from "../../routers/userRouter.js";
-
-const PORT =  5050;
-
-
+import cookieParser from 'cookie-parser'
+const PORT =  process.env.PORT || 5000;
 const app = express();
 
 app.use(json())
+app.use(cookieParser())
 app.use("/api/v1/user", userRouter)
 
 async function start()  {
     try {
-        await mongoose.connect('mongodb+srv://user:user@backend-starter.g7qtq.mongodb.net/USPECT?retryWrites=true&w=majority')
+        await mongoose.connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
          app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
     } catch (e) {
         console.log(e);
